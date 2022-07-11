a63a9c6ac64ffdab4ea2fcdfff45bf314d445989
Annie Pulzone <ajpulzone@gmail.com>
2022-07-10 20:37:14 -0700

    Called arrays

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,8 @@ food = ["Tacos", "Pizza", "Ice Cream", "Pickles", "Salad"]
 amount = [4, 6, 203, 17, 82, 37]
 weights = [13.2, 43.2, 1.7, 18.4, 77.3]
 facts = [true, false, false, true, false]
+
+puts food[3]
+puts amount[0]
+puts weights[2]
+puts facts.pop
