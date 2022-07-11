567f0fdafeec77924ed506267a916494f27ee600
Annie Pulzone <ajpulzone@gmail.com>
2022-07-11 07:00:02 -0700

    Added pop

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,9 @@ food = ["Tacos", "Pizza", "Ice Cream", "Pickles", "Chicken"]
 ages = [16, 84, 37, 22, 54]
 weights = [110.6, 38.4, 180.9, 215.3, 154.3]
 answers = [true, false, false, false, true]
+
+# This line of code calls the `pop` method on the food Array. This will remove
+# the last element within the food Array --> "Chicken"
+puts food
+puts food.pop
+puts food
